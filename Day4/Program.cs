1ce2380f534047a1b005c48410f0dacc5377cefd
lewy054 <norbert.lewicz1@gmail.com>
2025-12-04 23:56:34 +0100

    Add day4 part1

---
+++ Day4/Program.cs
@@ -0,0 +1,9 @@
+﻿using Day4;
+using Infrastructure;
+
+const string fileName = "input.txt";
+var input = FileHelpers.GetFileContent(fileName).ToList();
+
+var part1 = new Part1(input);
+var accessiblePaperRolls = part1.Resolve();
+Console.WriteLine($"Results of Day1, Part1 : {accessiblePaperRolls}");
---
+++ Infrastructure/GridUtils.cs
@@ -0,0 +1,37 @@
+﻿namespace Infrastructure;
+
+public static class GridUtils
+{
+    static readonly (int dx, int dy)[] NeighborOffsets =
+    [
+        (-1, -1), (0, -1), (1, -1),
+        (-1, 0), (1, 0),
+        (-1, 1), (0, 1), (1, 1)
+    ];
+
+    public static int CountNeighbors(this List<string> grid, int x, int y, char target)
+    {
+        var count = 0;
+
+        foreach (var (dx, dy) in NeighborOffsets)
+        {
+            var nx = x + dx;
+            var ny = y + dy;
+
+            if (ny < 0 || ny >= grid.Count)
+            {
+                continue;
+            }
+
+            if (nx < 0 || nx >= grid[ny].Length)
+            {
+                continue;
+            }
+
+            if (grid[ny][nx] == target)
+                count++;
+        }
+
+        return count;
+    }
+}
