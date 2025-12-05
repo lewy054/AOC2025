9b1841924cc572baa1eb8080345d2edd46c6fb48
lewy054 <norbert.lewicz1@gmail.com>
2025-12-05 15:51:17 +0100

    Add day5

---
+++ Day5/Program.cs
@@ -0,0 +1,29 @@
+﻿using Day5;
+using Infrastructure;
+
+const string fileName = "input.txt";
+var input = FileHelpers.GetFileContent(fileName).ToList();
+
+var separatorIndex = input.FindIndex(string.IsNullOrWhiteSpace);
+var freshIngredientsRanges = input.Take(separatorIndex).ToList();
+var availableIngredientIds = input.Skip(separatorIndex + 1).ToList();
+var refrigerator = new Refrigerator();
+
+foreach (var freshIngredientsRange in freshIngredientsRanges)
+{
+    var ranges = freshIngredientsRange.Split("-");
+    var from = ulong.Parse(ranges[0]);
+    var to = ulong.Parse(ranges[1]);
+    refrigerator.PutFreshIngredientIdRange(from, to);
+}
+
+foreach (var availableIngredientId in availableIngredientIds)
+{
+    refrigerator.AddIngredientId(ulong.Parse(availableIngredientId));
+}
+
+var freshIngredientsCount = refrigerator.GetFreshIngredientsCount();
+Console.WriteLine($"Results of Day5, Part1 : {freshIngredientsCount}");
+
+var freshIngredientsBasedOnRangesCount = refrigerator.GetFreshIngredientsBasedOnRangesCount();
+Console.WriteLine($"Results of Day5, Part2 : {freshIngredientsBasedOnRangesCount}");
---
+++ Day5/Refrigerator.cs
@@ -0,0 +1,69 @@
+﻿namespace Day5;
+
+public class Refrigerator
+{
+    private List<(ulong from, ulong to)> FreshIngredientsIdsRange { get; } = [];
+    private List<ulong> IngredientIds { get; } = [];
+
+    public void PutFreshIngredientIdRange(ulong from, ulong to)
+    {
+        FreshIngredientsIdsRange.Add((from, to));
+    }
+
+    public void AddIngredientId(ulong id)
+    {
+        IngredientIds.Add(id);
+    }
+
+    public int GetFreshIngredientsCount()
+    {
+        return GetFreshIngredients().Count;
+    }
+
+    public List<ulong> GetFreshIngredients()
+    {
+        var freshIngredientsIds = new List<ulong>();
+        foreach (var productId in IngredientIds)
+        {
+            foreach (var (from, to) in FreshIngredientsIdsRange)
+            {
+                if (productId < from || productId > to)
+                {
+                    continue;
+                }
+                freshIngredientsIds.Add(productId);
+                break;
+            }
+        }
+        return freshIngredientsIds;
+    }
+
+    public ulong GetFreshIngredientsBasedOnRangesCount()
+    {
+        var ranges = FreshIngredientsIdsRange
+            .OrderBy(r => r.from)
+            .ToList();
+        ulong totalFreshIngredientsCount = 0;
+        var rangeStart = ranges.First().from;
+        var rangeEnd = ranges.First().to;
+
+        for (var i = 1; i < ranges.Count; i++)
+        {
+            var (from, to) = ranges[i];
+            if (from <= rangeEnd + 1)
+            {
+                //ranges can be merged
+                rangeEnd = Math.Max(rangeEnd, to);
+            }
+            else
+            {
+                totalFreshIngredientsCount += rangeEnd - rangeStart + 1;
+                rangeStart = from;
+                rangeEnd = to;
+            }
+        }
+        totalFreshIngredientsCount += rangeEnd - rangeStart + 1;
+        return totalFreshIngredientsCount;
+    }
+
+}
