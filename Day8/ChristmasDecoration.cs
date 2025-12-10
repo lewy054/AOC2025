792d8473d928f7e888f347797eaf468ffe4c26b6
lewy054 <norbert.lewicz1@gmail.com>
2025-12-10 15:47:49 +0100

    add day 8 part2

--- Day8/ChristmasDecoration.cs
+++ Day8/ChristmasDecoration.cs
@@ -28,7 +28,8 @@ public class ChristmasDecoration
         {
             for (var j = i + 1; j < JunctionBoxes.Count; j++)
             {
-                var distance = Distance.CalculateDistanceBetweenTwoPoints(JunctionBoxes[i].X, JunctionBoxes[i].Y, JunctionBoxes[i].Z,
+                var distance = Distance.CalculateDistanceBetweenTwoPoints(JunctionBoxes[i].X, JunctionBoxes[i].Y,
+                    JunctionBoxes[i].Z,
                     JunctionBoxes[j].X, JunctionBoxes[j].Y, JunctionBoxes[j].Z);
                 junctionDistances.Add(new JunctionDistance()
                 {
@@ -42,36 +43,91 @@ public class ChristmasDecoration
         return junctionDistances;
     }
 
-    public static List<List<JunctionBox>> GetShortestJunctionDistances(List<JunctionDistance> distances, int howManyShortestConnections)
+    public List<List<JunctionBox>> GetCircuitsAfterNConnections(List<JunctionDistance> distances, int howMany)
     {
         var circuits = new List<List<JunctionBox>>();
-        var orderedJunctionDistances = distances.OrderBy(e => e.StraightLineDistance).ToList();
-        for (var i = 0; i < howManyShortestConnections; i++)
+        var ordered = distances.OrderBy(d => d.StraightLineDistance).ToList();
+
+        for (var i = 0; i < howMany && i < ordered.Count; i++)
         {
-            var circuit1 = circuits.FirstOrDefault(l => l.Contains(orderedJunctionDistances[i].Junction1));
-            var circuit2 = circuits.FirstOrDefault(l => l.Contains(orderedJunctionDistances[i].Junction2));
+            circuits = GetCircuits(circuits, ordered[i]);
+        }
+
+        return circuits;
+    }
+
+    public static List<List<JunctionBox>> GetCircuits(List<List<JunctionBox>> circuits, JunctionDistance distance)
+    {
+        var circuit1 = circuits.FirstOrDefault(l => l.Contains(distance.Junction1));
+        var circuit2 = circuits.FirstOrDefault(l => l.Contains(distance.Junction2));
+        if (circuit1 != null && circuit2 != null)
+        {
+            if (circuit1 != circuit2)
+            {
+                circuit1.AddRange(circuit2);
+                circuits.Remove(circuit2);
+            }
+        }
+        else if (circuit1 != null)
+        {
+            circuit1.Add(distance.Junction2);
+        }
+        else if (circuit2 != null)
+        {
+            circuit2.Add(distance.Junction1);
+        }
+        else
+        {
+            circuits.Add([distance.Junction1, distance.Junction2]);
+        }
+
+
+        return circuits;
+    }
+
+    public (JunctionBox Junction1, JunctionBox Junction2) GetLastConnection(
+        List<JunctionDistance> junctionDistances)
+    {
+        var orderedDistances = junctionDistances.OrderBy(d => d.StraightLineDistance).ToList();
+        var circuits = new List<List<JunctionBox>>();
+
+        foreach (var dist in orderedDistances)
+        {
+            var circuit1 = circuits.FirstOrDefault(c => c.Contains(dist.Junction1));
+            var circuit2 = circuits.FirstOrDefault(c => c.Contains(dist.Junction2));
+
             if (circuit1 != null && circuit2 != null)
             {
                 if (circuit1 != circuit2)
                 {
                     circuit1.AddRange(circuit2);
                     circuits.Remove(circuit2);
+                    var allCircuitsCount = circuits.Sum(c => c.Count);
+                    if (circuits.Count == 1 && allCircuitsCount == JunctionBoxes.Count)
+                    {
+                        return (dist.Junction1, dist.Junction2);
+                    }
                 }
             }
             else if (circuit1 != null)
             {
-                circuit1.Add(orderedJunctionDistances[i].Junction2);
+                circuit1.Add(dist.Junction2);
             }
             else if (circuit2 != null)
             {
-                circuit2.Add(orderedJunctionDistances[i].Junction1);
+                circuit2.Add(dist.Junction1);
             }
             else
             {
-                circuits.Add([orderedJunctionDistances[i].Junction1, orderedJunctionDistances[i].Junction2]);
+                circuits.Add([dist.Junction1, dist.Junction2]);
+            }
+            var allCircuitsCountSum = circuits.Sum(c => c.Count);
+            if (circuits.Count == 1 && allCircuitsCountSum == JunctionBoxes.Count)
+            {
+                return (dist.Junction1, dist.Junction2);
             }
         }
 
-        return circuits;
+        throw new InvalidOperationException("Could not find the last connection; the circuits never connect.");
     }
 }
--- Day8/Program.cs
+++ Day8/Program.cs
@@ -8,10 +8,16 @@ const int howManyShortestConnections = 1000;
 var decoration = new ChristmasDecoration();
 decoration.InitializeJunctionBoxes(input);
 var junctionDistances = decoration.CalculateDistanceBetweenJunctionBoxes();
-var shortestConnections = ChristmasDecoration.GetShortestJunctionDistances(junctionDistances, howManyShortestConnections);
+var shortestConnections = decoration.GetCircuitsAfterNConnections(junctionDistances, howManyShortestConnections);
 var orderedShortestConnections = shortestConnections.OrderByDescending(e => e.Count);
 var threeLargestCircuits = orderedShortestConnections.Take(3).ToList();
 var result = threeLargestCircuits.Select(e=> e.Count).Aggregate((a, b) => a * b);
 
 Console.WriteLine($"Results of Day8, Part1 : {result}");
+
+var lastConnection = decoration.GetLastConnection(junctionDistances);
+var result1 = (long)lastConnection.Junction1.X * (long)lastConnection.Junction2.X;
+
+Console.WriteLine($"Results of Day8, Part2 : {result1}");
+
 
